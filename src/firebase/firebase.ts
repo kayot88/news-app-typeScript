2f45a94a209c1d667b15265de45434137fb7f42a
Kayot88 <kayot88@gmail.com>
2019-10-30 14:12:32 +0200

    deploy_netlify

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: 'AIzaSyBqki_-vKU79HWX8hpMvzCPXQrf-gm2ZUY',
+  authDomain: 'news-app-ts.firebaseapp.com',
+  databaseURL: 'https://news-app-ts.firebaseio.com',
+  projectId: 'news-app-ts',
+  storageBucket: 'news-app-ts.appspot.com',
+  messagingSenderId: '239459019582',
+  appId: '1:239459019582:web:ea094baa53928f299cda6f'
 };
 
 if (!firebase.apps.length) {
